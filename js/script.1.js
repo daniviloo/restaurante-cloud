1d5db1ff06ec88492c93b936a6e85f49f7a7af88
Daniela Villalobos <d.villalobos006@ebc.edu.mx>
2026-04-20 08:32:39 -0600

    Update script.1.js

--- js/script.1.js
+++ js/script.1.js
@@ -7,7 +7,7 @@ const menu = [
     id: 1,
     name: "Tagliere Toscano",
     category: "entradas",
-    image: "img/carpaccio.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/pasta.jpg",
     description: "Selección de prosciutto, salames artesanales y quesos curados con pan rústico.",
     price: "$125"
   },
@@ -15,7 +15,7 @@ const menu = [
     id: 2,
     name: "Antipasto della Casa",
     category: "entradas",
-    image: "img/risotto.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/ensalada.jpg",
     description: "Verduras asadas, aceitunas marinadas y quesos típicos de la Toscana.",
     price: "$98"
   },
@@ -23,7 +23,7 @@ const menu = [
     id: 3,
     name: "Bruschetta al Pomodoro",
     category: "entradas",
-    image: "img/pollo.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/bruschetta.jpg",
     description: "Pan artesanal con tomate fresco, albahaca y aceite de oliva extra virgen.",
     price: "$78"
   },
@@ -31,7 +31,7 @@ const menu = [
     id: 4,
     name: "Carpaccio Fiorentino",
     category: "entradas",
-    image: "img/carpaccio.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/carpaccio.jpg",
     description: "Láminas de res marinadas con limón, rúcula y escamas de parmesano.",
     price: "$155"
   },
@@ -41,7 +41,7 @@ const menu = [
     id: 5,
     name: "Lasagna alla Bolognese",
     category: "Plato Fuerte",
-    image: "img/lasagna.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/lasagna.jpg",
     description: "Pasta horneada con ragú tradicional, salsa bechamel y queso gratinado.",
     price: "$195"
   },
@@ -49,7 +49,7 @@ const menu = [
     id: 6,
     name: "Risotto ai Funghi",
     category: "Plato Fuerte",
-    image: "img/risotto.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/risotto.jpg",
     description: "Arroz cremoso con hongos salteados, vino blanco y parmesano.",
     price: "$185"
   },
@@ -57,7 +57,7 @@ const menu = [
     id: 7,
     name: "Pizza Rustica",
     category: "Plato Fuerte",
-    image: "img/pizzam.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/pizzam.jpg",
     description: "Masa fina con salsa de tomate, mozzarella fresca y especias italianas.",
     price: "$165"
   },
@@ -65,7 +65,7 @@ const menu = [
     id: 8,
     name: "Pollo al Rosmarino",
     category: "Plato Fuerte",
-    image: "img/pollo.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/pollo.jpg",
     description: "Pechuga de pollo cocinada con romero, limón y aceite de oliva.",
     price: "$175"
   },
@@ -75,7 +75,7 @@ const menu = [
     id: 9,
     name: "Tiramisù della Nonna",
     category: "postres",
-    image: "img/tiramisú.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/tiramisú.jpg",
     description: "Café intenso, cacao y crema de mascarpone en capas suaves.",
     price: "$99"
   },
@@ -83,7 +83,7 @@ const menu = [
     id: 10,
     name: "Panna Cotta ai Frutti Rossi",
     category: "postres",
-    image: "img/panna.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/panna.jpg",
     description: "Panna cotta cremosa con coulis de frutos rojos frescos.",
     price: "$92"
   },
@@ -91,7 +91,7 @@ const menu = [
     id: 11,
     name: "Cannoli Tradizionali",
     category: "postres",
-    image: "img/cannoli.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/cannoli.jpg",
     description: "Tubos crujientes rellenos con ricotta dulce y ralladura de naranja.",
     price: "$105"
   },
@@ -99,7 +99,7 @@ const menu = [
     id: 12,
     name: "Gelato Italiano",
     category: "postres",
-    image: "img/gelato.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/gelato.jpg",
     description: "Helado artesanal disponible en chocolate, pistache o fior di latte.",
     price: "$88"
   },
@@ -109,7 +109,7 @@ const menu = [
     id: 13,
     name: "Limonata Siciliana",
     category: "bebidas",
-    image: "img/limonada.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/limonada.jpg",
     description: "Bebida fresca con limón siciliano, menta y agua con gas.",
     price: "$62"
   },
@@ -117,7 +117,7 @@ const menu = [
     id: 14,
     name: "Cappuccino Cremoso",
     category: "bebidas",
-    image: "img/cappuccino.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/cappuccino.jpg",
     description: "Espresso intenso con leche vaporizada y espuma ligera.",
     price: "$68"
   },
@@ -125,7 +125,7 @@ const menu = [
     id: 15,
     name: "Vino Rosso della Casa",
     category: "bebidas",
-    image: "img/vino.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/vino.jpg",
     description: "Copa de vino tinto italiano con notas afrutadas.",
     price: "$125"
   },
@@ -133,7 +133,7 @@ const menu = [
     id: 16,
     name: "Aperol Spritz",
     category: "bebidas",
-    image: "img/Aperol.jpg",
+    image: "https://tiendacloud01.blob.core.windows.net/imagenesdanny/Aperol.jpg",
     description: "Prosecco, Aperol y soda en la mezcla clásica italiana.",
     price: "$115"
   }
